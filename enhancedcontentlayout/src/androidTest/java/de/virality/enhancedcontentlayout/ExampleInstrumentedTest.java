1edb268e0274cd926471c25014ad46db76af1751
Benjamin Bisinger <bisinger@virality.de>
2016-10-27 15:00:39 +0200

    config for bintray

--- enhancedcontentlayout/src/androidTest/java/de/virality/enhancedcontentlayout/ExampleInstrumentedTest.java
+++ enhancedcontentlayout/src/androidTest/java/de/virality/enhancedcontentlayout/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("de.virality.enhancedcontentlayout", appContext.getPackageName());
+        assertEquals("de.virality.enhancedcontentlayout.test", appContext.getPackageName());
     }
 }
